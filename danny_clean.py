78217e560f9fef047b194c2520765adf9f186afb
Danny Prikazsky <dannyprikaz@gmail.com>
2016-06-29 11:21:51 -0700

    commented on cleaner

--- danny_clean.py
+++ danny_clean.py
@@ -2,5 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('data/churn.csv')
 
+# Dummify the city column and drop 'city'
+df[['Astapor', "King's Landing", 'Winterfell']] = pd.get_dummies(df['city'])
+df = df.drop(['city'], axis=1)
+
+# Drop rows that have NaN values
 df = df[df['avg_rating_by_driver'].notnull()]
 df = df[df['avg_rating_of_driver'].notnull()]
